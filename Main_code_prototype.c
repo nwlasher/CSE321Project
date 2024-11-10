c9ac9f2a47edbe5180a99f40e7af1a4f35c189ef
nwlasher <nwlasher@buffalo.edu>
2024-11-10 14:46:16 -0500

    pushed c file version of main code to be visable from main directory

---
+++ Main_code_prototype.c
@@ -0,0 +1,41 @@
+#define TRIGGER 35 //Output value when the sensor is NOT triggered by manufacturer definition, I've inverted it here.
+//Init I/O pins
+int leftMotorPin = 4;
+int rightMotorPin = 2;
+int leftIrPin = A4;
+int rightIrPin = A5;
+void setup() {
+  //Initialize IR pins for input
+  pinMode(leftIrPin, INPUT);
+  pinMode(rightIrPin, INPUT);
+  //Initialize LED pins for output
+  pinMode(leftMotorPin, OUTPUT);
+  pinMode(rightMotorPin, OUTPUT);
+//  Serial.begin(9600);
+}
+
+void loop() {
+  if((analogRead(rightIrPin) > TRIGGER) && (analogRead(leftIrPin) > TRIGGER)){//End tile found or device removed from course
+      digitalWrite(leftMotorPin, LOW);
+      digitalWrite(rightMotorPin, LOW);
+  }else if (analogRead(rightIrPin) > TRIGGER){//Adjust direction left
+      digitalWrite(rightMotorPin, HIGH);
+      digitalWrite(leftMotorPin, LOW);
+  }else if (analogRead(leftIrPin) > TRIGGER){//Adjust direction right
+      digitalWrite(leftMotorPin, HIGH);
+      digitalWrite(rightMotorPin, LOW);
+  }else{//Continue forward
+      digitalWrite(leftMotorPin, HIGH);  
+      digitalWrite(rightMotorPin, HIGH);
+  }
+  
+
+  
+//  //Monitor state here
+//  Serial.print("Right: ");
+//  Serial.print(rightObjectProximity);
+//  Serial.print(" Left: ");
+//  Serial.print(leftObjectProximity);
+//  Serial.print("\n");
+//  delay(100);
+}
